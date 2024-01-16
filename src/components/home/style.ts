911d3f0e973a8c3f7221974bf8472332c52855a7
daniloramin <daniloramin00@hotmail.com>
2024-01-15 21:52:33 -0300

    criacao base da home

--- src/components/home/style.ts
+++ src/components/home/style.ts
@@ -1,10 +1,54 @@
 import styled from "styled-components";
 
 export const StyledHome = styled.section`
-  border: 2px solid blue;
   height: 100vh;
   height: 100dvh;
 
   background-color: var(--primary);
   background-image: linear-gradient(to right, var(--primary), var(--secondary));
+
+  display: grid;
+  place-items: center;
+
+  div.container {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    justify-items: center;
+    align-items: center;
+    gap: 30px;
+
+    .left {
+      h1 {
+        font-size: 48px;
+      }
+    }
+
+    .right {
+      position: relative;
+      z-index: 1;
+      border-radius: 10px;
+
+      img {
+        max-width: 300px;
+        aspect-ratio: 1/1.5;
+        border-radius: inherit;
+        box-shadow: 0 10px 10px 0 rgba(0, 0, 0, 0.2);
+      }
+    }
+
+    @media (max-width: 750px) {
+      & {
+        grid-template-columns: 1fr;
+        text-align: center;
+      }
+    }
+
+    @media (max-width: 500px) {
+      .left {
+        h1 {
+          font-size: 30px;
+        }
+      }
+    }
+  }
 `;
---
+++ src/components/styled/styled.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const StyledBackgroundHighlight = styled.span`
+  background-color: var(--accent);
+  color: white;
+  font-size: inherit;
+  font-family: inherit;
+`;
