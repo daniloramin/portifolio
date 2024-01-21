a7d2858ac34017d24566dabd1879731b9ff2e2ea
daniloramin <daniloramin00@hotmail.com>
2024-01-21 16:40:59 -0300

    change infobox borders and animate home picture

--- src/components/about/style.ts
+++ src/components/about/style.ts
@@ -11,15 +11,26 @@ type StyledInfoBoxProps = {
 
 export const StyledInfoBox = styled.div<StyledInfoBoxProps>`
   position: relative;
-  border-radius: 1rem;
   padding: 3rem;
   margin-bottom: 5rem;
   max-width: 750px;
   background-color: white;
+  border: 1px solid ${(props) => (props.$secondary ? "var(--secondary)" : "var(--primary)")};
   color: var(--text);
 
+  /*
   box-shadow: ${(props) =>
     props.$secondary ? "10px 10px 0px 0 var(--secondary)" : "-10px 10px 0px 0 var(--primary)"};
+    */
+
+    
+    &::before {
+      content: '';
+      position: absolute;
+      border: 1px solid ${props => props.$secondary ? "var(--secondary)" : "var(--primary)"};
+      inset: ${props => props.$secondary ? "10px -10px -10px 10px" : "10px 10px -10px -10px"};
+      z-index: -1;
+    }
 
   margin-left: ${(props) => props.$secondary && "auto"};
   text-align: ${(props) => props.$secondary && "end"};
